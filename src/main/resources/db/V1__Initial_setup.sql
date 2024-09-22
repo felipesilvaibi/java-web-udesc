4b4b0872f5ea26216f35c02b1ddb3f7db75b2eae
root <root@Lipe>
2024-09-21 21:31:41 -0300

    chore: change migration v1

--- src/main/resources/db/V1__Initial_setup.sql
+++ src/main/resources/db/V1__Initial_setup.sql
@@ -1,57 +1,75 @@
--- DROP TABLES IF EXISTS (Esses comandos são comentados por segurança)
--- DROP TABLE IF EXISTS public.tb_movie_actor;
--- DROP TABLE IF EXISTS public.tb_actor;
--- DROP TABLE IF EXISTS public.tb_movie;
--- DROP TABLE IF EXISTS public.tb_customer;
+DROP TABLE IF EXISTS tb_rental;
+DROP TABLE IF EXISTS tb_stock;
+DROP TABLE IF EXISTS tb_movie_actor;
+DROP TABLE IF EXISTS tb_movie;
+DROP TABLE IF EXISTS tb_actor;
+DROP TABLE IF EXISTS tb_customer;
+
+-- Criação da tabela tb_actor
+CREATE TABLE tb_actor (
+    id UUID PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    nationality VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
 
 -- Criação da tabela tb_customer
-CREATE TABLE public.tb_customer (
-	id uuid NOT NULL,
-	address varchar(64) NOT NULL,
-	birth_date varchar(10) NOT NULL,
-	cpf varchar(11) NOT NULL,
-	created_at timestamp(6) NOT NULL,
-	email varchar(64) NOT NULL,
-	"name" varchar(32) NOT NULL,
-	phone varchar(11) NOT NULL,
-	updated_at timestamp(6) NOT NULL,
-	CONSTRAINT tb_customer_pkey PRIMARY KEY (id),
-	CONSTRAINT uk1r2x5kcodv7u39r4k8jfk78b8 UNIQUE (cpf),
-	CONSTRAINT uka9xeibptr987g1od1c430m1w9 UNIQUE (email),
-	CONSTRAINT ukf8602v7whiicyob59sabtu00u UNIQUE (phone)
+CREATE TABLE tb_customer (
+    id UUID PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    email VARCHAR(64) NOT NULL UNIQUE,
+    phone VARCHAR(11) NOT NULL UNIQUE,
+    address VARCHAR(64) NOT NULL,
+    birth_date VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
 
 -- Criação da tabela tb_movie
-CREATE TABLE public.tb_movie (
-	id uuid NOT NULL,
-	created_at timestamp(6) NOT NULL,
-	duration int4 NOT NULL,
-	genre varchar(64) NOT NULL,
-	synopsis varchar(512) NOT NULL,
-	title varchar(128) NOT NULL,
-	updated_at timestamp(6) NOT NULL,
-	CONSTRAINT tb_movie_pkey PRIMARY KEY (id)
+CREATE TABLE tb_movie (
+    id UUID PRIMARY KEY,
+    title VARCHAR(128) NOT NULL,
+    genre VARCHAR(64) NOT NULL,
+    synopsis VARCHAR(512) NOT NULL,
+    duration INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
 
--- Criação da tabela tb_actor
-CREATE TABLE public.tb_actor (
-	id uuid NOT NULL,
-	created_at timestamp(6) NOT NULL,
-	"name" varchar(32) NOT NULL,
-	nationality varchar(32) NOT NULL,
-	updated_at timestamp(6) NOT NULL,
-	CONSTRAINT tb_actor_pkey PRIMARY KEY (id)
+-- Criação da tabela tb_stock
+CREATE TABLE tb_stock (
+    id UUID PRIMARY KEY,
+    movie_id UUID NOT NULL UNIQUE,
+    quantity INTEGER NOT NULL,
+    rented INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES tb_movie(id) ON DELETE CASCADE
 );
 
--- Criação da tabela tb_movie_actor com delete cascade
-CREATE TABLE public.tb_movie_actor (
-    id uuid NOT NULL,
-    created_at timestamp(6) NOT NULL,
-    updated_at timestamp(6) NOT NULL,
-    actor_id uuid,
-    movie_id uuid,
-    CONSTRAINT tb_movie_actor_pkey PRIMARY KEY (id),
-    CONSTRAINT uko2gmiyt6f7arr7mw1a50h644p UNIQUE (movie_id, actor_id),
-    CONSTRAINT fkkc11oso89pvbyrwpg265bmj9v FOREIGN KEY (movie_id) REFERENCES public.tb_movie(id) ON DELETE CASCADE,
-    CONSTRAINT fknaiva749sdfsiyo4xsj3xajqi FOREIGN KEY (actor_id) REFERENCES public.tb_actor(id)
+-- Criação da tabela tb_movie_actor
+CREATE TABLE tb_movie_actor (
+    id UUID PRIMARY KEY,
+    movie_id UUID NOT NULL,
+    actor_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    UNIQUE (movie_id, actor_id),
+    FOREIGN KEY (movie_id) REFERENCES tb_movie(id) ON DELETE CASCADE,
+    FOREIGN KEY (actor_id) REFERENCES tb_actor(id) ON DELETE CASCADE
+);
+
+-- Criação da tabela tb_rental
+CREATE TABLE tb_rental (
+    id UUID PRIMARY KEY,
+    customer_id UUID NOT NULL,
+    stock_id UUID NOT NULL,
+    rental_date DATE NOT NULL,
+    return_date DATE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES tb_customer(id),
+    FOREIGN KEY (stock_id) REFERENCES tb_stock(id)
 );
